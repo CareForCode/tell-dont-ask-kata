0f17528ba7dfb48f103785cc4f916bf0c4d0bfff
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-11 08:55:57 +0100

    wip

--- java/src/main/java/it/gabrieletondi/telldontaskkata/useCase/request/OrderShipmentRequest.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/useCase/request/OrderShipmentRequest.java
@@ -1,13 +1,9 @@
 package it.gabrieletondi.telldontaskkata.useCase.request;
 
 public class OrderShipmentRequest {
-    private int orderId;
+    private final int orderId;
 
     public OrderShipmentRequest(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public void setOrderId(int orderId) {
         this.orderId = orderId;
     }
 
